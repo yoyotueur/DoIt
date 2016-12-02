d1a85a7169dde568997967626f7dff434232f857
Johan Bertin <johan.bertin@gmail.com>
2016-12-02 16:56:13 +0100

    Setting things up

---
+++ DoIt/CreateTaskViewController.swift
@@ -0,0 +1,31 @@
+//
+//  CreateTaskViewController.swift
+//  DoIt
+//
+//  Created by Johan Bertin on 02/12/2016.
+//  Copyright © 2016 yoyo. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    @IBOutlet weak var taskNameTextField: UITextField!
+    
+    @IBOutlet weak var importantSwitch: UISwitch!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func addTapped(_ sender: Any) {
+    }
+   
+    
+
+
+}
