d9478205bb23d07a88323a9c5cbcf096c2b9e4cb
Johan Bertin <johan.bertin@gmail.com>
2016-12-02 16:34:15 +0100

    Task Class

---
+++ DoIt/task.swift
@@ -0,0 +1,14 @@
+//
+//  task.swift
+//  DoIt
+//
+//  Created by Johan Bertin on 02/12/2016.
+//  Copyright © 2016 yoyo. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
